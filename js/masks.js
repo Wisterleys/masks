b3d2395246fa1c744c698c84ddd0eb5eccb1ebb2
Wisterleys <wister.sousa2@gmail.com>
2021-09-01 09:40:45 -0300

    mascara para porcento criada e funcionando

--- js/masks.js
+++ js/masks.js
@@ -38,6 +38,11 @@ const mask={
         return value.replace(/\D/g,'')
         .replace(/([\d]{5})(\d)/,'$1-$2')
         .replace(/(-[\d]{3})\d+?$/,'$1')
+    },
+    percent(value){
+        return value.replace(/\D/g,'')
+        .replace(/([\d]{2})(\d)/,'$1,$2')
+        .replace(/(,[\d]{2})\d+?$/,'$1')
     }
     
 }
